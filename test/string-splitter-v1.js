fb533187c2bdf876b054a8980eca91c9f2edc091
Gregor Deretic <gregor.deretic@sky.uk>
2021-01-29 14:22:53 +0000

    first commit

--- test/string-splitter-v1.js
+++ test/string-splitter-v1.js
@@ -1,3 +1,33 @@
 var assert = require("chai").assert;
 var expect = require("chai").expect;
 
+// Put your CUT here
+class StringSplitter
+{
+    splitString( stringToSplit )
+    {
+        var result = null;
+        
+        if (stringToSplit.length < 1)
+            result = []
+
+        return result;
+    }
+}
+
+describe('Splitting a string', function() {
+    it('Input an empty string, return an empty array []', function() {
+        // arrange...
+        var cut = new StringSplitter();
+        var expectedResult = []
+        var emptyString = "";
+
+        // act...
+        var actualResult = cut.splitString( emptyString );
+        console.log( actualResult.length);
+
+        // assert...
+        assert.equal(actualResult.length, expectedResult.length );
+        expect(actualResult).to.eql(expectedResult);
+    });
+});
